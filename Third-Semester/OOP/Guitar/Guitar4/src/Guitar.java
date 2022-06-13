3c4feea23b51b29661b6f5a364f2ccf53c4a5371
Gianluca Starke <gianlucastarke@gmail.com>
2022-06-13 15:23:06 -0300

    update

--- Third-Semester/OOP/Guitar/Guitar4/src/Guitar.java
+++ Third-Semester/OOP/Guitar/Guitar4/src/Guitar.java
@@ -1,11 +1,8 @@
-public class Guitar{
-    private String serialNumber;
-    private double price;    
+public class Guitar extends Instrument{
     private GuitarSpec spec;
 
     public Guitar(String serialNumber, double price, GuitarSpec spec){
-        this.serialNumber = serialNumber;
-        this.price = price;
+        super(serialNumber, price);
         this.spec = spec;
     }
 
--- Third-Semester/OOP/Guitar/Guitar4/src/Instrument.java
+++ Third-Semester/OOP/Guitar/Guitar4/src/Instrument.java
@@ -2,6 +2,11 @@ public class Instrument {
     private String serialNumber;
     private double price;
 
+    public Instrument(String serialNumber, double price){
+        this.serialNumber = serialNumber;
+        this.price = price;
+    }
+
     public void setPrice(float price){
         this.price = price;
     }
