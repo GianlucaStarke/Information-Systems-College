58dc2b868bfd927b13d43e75eb861e93502a0a27
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-09-29 13:31:01 -0300

    Commit esfera

--- Forth-Semester/Data-Structure/List1/Esfera/esfera.c
+++ Forth-Semester/Data-Structure/List1/Esfera/esfera.c
@@ -1,8 +1,11 @@
 #include "esfera.h"
 
+// Constante PI
 const double PI = 3.141593;
 
+// Constructor do tipo estruturado Esfera
 Esfera* cria(double diametro){
+    // Aloca memória
     Esfera* esfera = (Esfera*) malloc(sizeof(Esfera));
     
     esfera->diametro = diametro;
@@ -10,14 +13,17 @@ Esfera* cria(double diametro){
     return esfera;
 }
 
+// Raio da esfera: r = d / 2
 double raio(Esfera* esfera){
     return esfera->diametro / 2;
 }
 
+// Area da esfera: 4 * pi * raio²
 double area(Esfera* esfera){
     return 4.0 * PI * pow(raio(esfera), 2);
 }
 
+// Volume da esfera: (4 * pi * raio³) / 3
 double volume(Esfera* esfera){
     return (4.0 * PI * pow(raio(esfera), 3)) / 3.0;
 }
--- Forth-Semester/Data-Structure/List1/Esfera/esfera.h
+++ Forth-Semester/Data-Structure/List1/Esfera/esfera.h
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 #include <math.h>
 
+// Tipo estruturado Esfera
 typedef struct{
     double diametro;
 }Esfera;
 
+// Constructor do tipo estruturado Esfera
 Esfera* cria(double diametro);
 
+// Raio da esfera: r = d / 2
 double volume(Esfera* esfera);
 
+// Area da esfera: 4 * pi * raio²
 double raio(Esfera* esfera);
 
+// Volume da esfera: (4 * pi * raio³) / 3
 double area(Esfera* esfera);
