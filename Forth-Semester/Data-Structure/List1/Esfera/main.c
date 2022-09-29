1c6744087fc604f67f22c54fd0f3f11d07fd3ddb
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-09-29 13:54:28 -0300

    Commit esfera

--- Forth-Semester/Data-Structure/List1/Esfera/main.c
+++ Forth-Semester/Data-Structure/List1/Esfera/main.c
@@ -1,8 +1,11 @@
 #include "esfera.h"
 
 int main(){
+    // Esfera
     Esfera* esfera;
+    // Diametro para atribuir a esfera
     double diametro;
+    // Variavel que controla o loop
     char continuar = 's';
 
     while(continuar == 's'){
@@ -11,8 +14,10 @@ int main(){
         
         printf("\n");
 
+        // Cria esfera
         esfera = cria(diametro);
 
+        // Imprime o raio, a area e o volume da esfera
         printf("-----------------------\n");
         printf("-Informações da esfera-\n");
         printf("-----------------------\n");
@@ -20,6 +25,7 @@ int main(){
         printf("Area:   %.2lf\n", area(esfera));
         printf("Volume: %.2lf\n\n", volume(esfera));
 
+        // Digite s para calcular mais uma esfera
         printf("Deseja inserir mais uma esfera?(s/n) ");
         scanf(" %c", &continuar);
         
