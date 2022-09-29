bb172bce7abe5c301fcb1dbb8e52c522356b0ff2
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-09-29 10:49:47 -0300

    Commit esfera

--- Forth-Semester/Data-Structure/List1/Esfera/main.c
+++ Forth-Semester/Data-Structure/List1/Esfera/main.c
@@ -8,16 +8,22 @@ int main(){
     while(1){
         printf("Insira o diâmetro da esfera: ");
         scanf("%lf", &diametro);
+        
+        printf("\n");
 
         esfera = cria(diametro);
 
-        printf("--------Informações da esfera--------\n");
-        printf("Raio: %lf\n", raio(esfera));
-        printf("Area: %lf\n", area(esfera));
-        printf("Volume: %lf\n\n", volume(esfera));
+        printf("-----------------------\n");
+        printf("-Informações da esfera-\n");
+        printf("-----------------------\n");
+        printf("Raio:   %.2lf\n", raio(esfera));
+        printf("Area:   %.2lf\n", area(esfera));
+        printf("Volume: %.2lf\n\n", volume(esfera));
 
         printf("Deseja inserir mais uma esfera?(s/n) ");
         scanf("%c", &continuar[0]);
+        
+        printf("\n");
 
         if(continuar[0] != 's') break;
     }
